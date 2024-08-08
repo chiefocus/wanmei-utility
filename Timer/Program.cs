d8e3d65f70fb47f3c5152f2f22f99199066e129b
Feng Chen <chenfeng1104@gmail.com>
2024-08-07 21:05:57 -0400

    prevent multiple instance

--- Timer/Program.cs
+++ Timer/Program.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Timer
 {
     internal static class Program
     {
+        private static readonly string appGuid = "B54548B9-FE81-4242-B551-174D808FBE36";
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Timer());
+            using (Mutex mutex = new Mutex(false, "Global\\" + appGuid))
+            {
+                if (!mutex.WaitOne(0, false))
+                {
+                    //MessageBox.Show("Instance already running");
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Timer());
+            }
         }
     }
 }
