85d5fb43623763e2756e7b477631ae39ec6364ab
unknown <Feng Chen>
2025-09-18 23:19:25 -0400

    resize3

--- Timer/SkillControl.Designer.cs
+++ Timer/SkillControl.Designer.cs
@@ -81,13 +81,14 @@ namespace TimerUtility
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.Control;
-            this.textBox2.Font = new System.Drawing.Font("SimHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("SimHei", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.textBox2.ForeColor = System.Drawing.Color.DarkGreen;
-            this.textBox2.Location = new System.Drawing.Point(82, 6);
+            this.textBox2.Location = new System.Drawing.Point(83, 11);
             this.textBox2.Margin = new System.Windows.Forms.Padding(1);
             this.textBox2.MaxLength = 3;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(26, 22);
+            this.textBox2.Size = new System.Drawing.Size(26, 15);
             this.textBox2.TabIndex = 4;
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
