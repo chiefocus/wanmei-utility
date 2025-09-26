44e848ee405c95ff591ce2b3d0871aba6cdd1a33
Feng Chen <feng.chen@morningstar.com>
2025-09-26 18:09:54 -0400

    poc

---
+++ WinformTimerGroups/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WinformTimerGroups
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private FlowLayoutPanel flowCategories;
+        private FlowLayoutPanel flowGroups;
+        private FlowLayoutPanel flowTimers;
+
+        /// <summary>
+        /// 清理资源
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.flowCategories = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowGroups = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowTimers = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelTitle.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowCategories
+            // 
+            this.flowCategories.AutoScroll = true;
+            this.flowCategories.Location = new System.Drawing.Point(279, 249);
+            this.flowCategories.Name = "flowCategories";
+            this.flowCategories.Size = new System.Drawing.Size(800, 50);
+            this.flowCategories.TabIndex = 2;
+            // 
+            // flowGroups
+            // 
+            this.flowGroups.AutoScroll = true;
+            this.flowGroups.Location = new System.Drawing.Point(279, 348);
+            this.flowGroups.Name = "flowGroups";
+            this.flowGroups.Size = new System.Drawing.Size(800, 50);
+            this.flowGroups.TabIndex = 1;
+            // 
+            // flowTimers
+            // 
+            this.flowTimers.AutoScroll = true;
+            this.flowTimers.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowTimers.Location = new System.Drawing.Point(354, 465);
+            this.flowTimers.Name = "flowTimers";
+            this.flowTimers.Size = new System.Drawing.Size(283, 202);
+            this.flowTimers.TabIndex = 0;
+            this.flowTimers.WrapContents = false;
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.Controls.Add(this.pictureBox1);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(0, 0);
+            this.panelTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(1439, 181);
+            this.panelTitle.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(7, 3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(3);
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(1439, 1046);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.flowTimers);
+            this.Controls.Add(this.flowGroups);
+            this.Controls.Add(this.flowCategories);
+            this.Name = "MainForm";
+            this.Text = "循环倒计时多层计时器";
+            this.panelTitle.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panelTitle;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ WinformTimerGroups/MainForm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WinformTimerGroups
+{
+    public partial class MainForm : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        private void panelTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        private Config config;
+        private Timer uiTimer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Load += MainForm_Load;
+            //this.Icon = Properties.Resources.timer;
+        }
+
+        private async void MainForm_Load(object sender, EventArgs e)
+        {
+            flowCategories.Enabled = false;
+
+            // 异步加载 XML
+            config = await System.Threading.Tasks.Task.Run(() => ConfigLoader.Load("config.xml"));
+
+            flowCategories.Enabled = true;
+
+            uiTimer = new Timer { Interval = 50 };
+            uiTimer.Start();
+
+            LoadCategories();
+        }
+
+        private void LoadCategories()
+        {
+            flowCategories.Controls.Clear();
+            foreach (var cat in config.Categories)
+            {
+                var btn = new CategoryButton(cat, config.Style.CategoryButton);
+                btn.CategoryClicked += (c) => LoadGroups(c);
+                flowCategories.Controls.Add(btn);
+            }
+        }
+
+        private void LoadGroups(Category cat)
+        {
+            flowGroups.Controls.Clear();
+            foreach (var grp in cat.Groups)
+            {
+                var btn = new GroupButton(grp, config.Style.GroupButton);
+                btn.GroupClicked += (g) => LoadTimers(g);
+                flowGroups.Controls.Add(btn);
+            }
+        }
+
+
+        private void LoadTimers(Group grp)
+        {
+            flowTimers.Controls.Clear();
+            var timerControls = new List<TimerControl>();
+
+            // 创建 TimerControl
+            foreach (var t in grp.Timers)
+            {
+                var ctl = new TimerControl(t, uiTimer, config.Style.TimerButton);
+                flowTimers.Controls.Add(ctl);
+                timerControls.Add(ctl);
+            }
+
+            // 建立 StartWith 关联
+            for (int i = 0; i < grp.Timers.Count; i++)
+            {
+                var startWith = grp.Timers[i].StartWith;
+                if (!string.IsNullOrEmpty(startWith))
+                {
+                    var names = startWith.Split(',');
+                    foreach (var name in names)
+                    {
+                        var linked = timerControls.Find(c => c.ButtonText == name.Trim());
+                        if (linked != null) timerControls[i].AddLinkedTimer(linked);
+                    }
+                }
+            }
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
