6777c47064245262f494715afbb4ae9eba0d92f5
Feng Chen <chenfeng1104@gmail.com>
2025-10-14 17:23:51 -0400

    clean up

--- Timer/Timer.Designer.cs
+++ Timer/Timer.Designer.cs
@@ -28,134 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WanmeiTimer));
-            timer = new System.Windows.Forms.Timer(components);
-            titlePanel = new System.Windows.Forms.Panel();
-            startButton = new System.Windows.Forms.Button();
-            udfButton = new System.Windows.Forms.RadioButton();
-            nowText = new System.Windows.Forms.TextBox();
-            instancePanel = new System.Windows.Forms.FlowLayoutPanel();
-            bossPanel = new System.Windows.Forms.FlowLayoutPanel();
-            skillPanel = new System.Windows.Forms.FlowLayoutPanel();
-            titlePanel.SuspendLayout();
-            SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.titlePanel = new System.Windows.Forms.Panel();
+            this.startButton = new System.Windows.Forms.Button();
+            this.udfButton = new System.Windows.Forms.RadioButton();
+            this.nowText = new System.Windows.Forms.TextBox();
+            this.instancePanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.bossPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.skillPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.titlePanel.SuspendLayout();
+            this.SuspendLayout();
             // 
             // timer
             // 
-            timer.Tick += new System.EventHandler(timer_Tick);
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // titlePanel
             // 
-            titlePanel.AutoScroll = true;
-            titlePanel.Controls.Add(startButton);
-            titlePanel.Controls.Add(udfButton);
-            titlePanel.Controls.Add(nowText);
-            titlePanel.Dock = System.Windows.Forms.DockStyle.Top;
-            titlePanel.Location = new System.Drawing.Point(3, 3);
-            titlePanel.Margin = new System.Windows.Forms.Padding(2);
-            titlePanel.Name = "titlePanel";
-            titlePanel.Size = new System.Drawing.Size(289, 32);
-            titlePanel.TabIndex = 0;
-            titlePanel.DoubleClick += new System.EventHandler(titlePanel_DoubleClick);
+            this.titlePanel.AutoScroll = true;
+            this.titlePanel.Controls.Add(this.startButton);
+            this.titlePanel.Controls.Add(this.udfButton);
+            this.titlePanel.Controls.Add(this.nowText);
+            this.titlePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.titlePanel.Location = new System.Drawing.Point(3, 3);
+            this.titlePanel.Margin = new System.Windows.Forms.Padding(2);
+            this.titlePanel.Name = "titlePanel";
+            this.titlePanel.Size = new System.Drawing.Size(278, 32);
+            this.titlePanel.TabIndex = 0;
+            this.titlePanel.DoubleClick += new System.EventHandler(this.titlePanel_DoubleClick);
             // 
             // startButton
             // 
-            startButton.Font = new System.Drawing.Font("SimHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            startButton.Location = new System.Drawing.Point(162, 2);
-            startButton.Margin = new System.Windows.Forms.Padding(0);
-            startButton.Name = "startButton";
-            startButton.Size = new System.Drawing.Size(64, 30);
-            startButton.TabIndex = 3;
-            startButton.Text = "开打计时";
-            startButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            startButton.UseVisualStyleBackColor = true;
-            startButton.Visible = false;
-            startButton.Click += new System.EventHandler(startButton_Click);
+            this.startButton.Font = new System.Drawing.Font("SimHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(162, 2);
+            this.startButton.Margin = new System.Windows.Forms.Padding(0);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(64, 30);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "开打计时";
+            this.startButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Visible = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // udfButton
             // 
-            udfButton.Appearance = System.Windows.Forms.Appearance.Button;
-            udfButton.Font = new System.Drawing.Font("SimHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            udfButton.Location = new System.Drawing.Point(110, 2);
-            udfButton.Margin = new System.Windows.Forms.Padding(0);
-            udfButton.Name = "udfButton";
-            udfButton.Size = new System.Drawing.Size(51, 30);
-            udfButton.TabIndex = 2;
-            udfButton.TabStop = false;
-            udfButton.Text = "自定义";
-            udfButton.UseVisualStyleBackColor = true;
-            udfButton.Click += new System.EventHandler(udfButton_Click);
+            this.udfButton.Appearance = System.Windows.Forms.Appearance.Button;
+            this.udfButton.Font = new System.Drawing.Font("SimHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.udfButton.Location = new System.Drawing.Point(110, 2);
+            this.udfButton.Margin = new System.Windows.Forms.Padding(0);
+            this.udfButton.Name = "udfButton";
+            this.udfButton.Size = new System.Drawing.Size(51, 30);
+            this.udfButton.TabIndex = 2;
+            this.udfButton.Text = "自定义";
+            this.udfButton.UseVisualStyleBackColor = true;
+            this.udfButton.Click += new System.EventHandler(this.udfButton_Click);
             // 
             // nowText
             // 
-            nowText.BackColor = System.Drawing.SystemColors.Control;
-            nowText.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            nowText.Cursor = System.Windows.Forms.Cursors.No;
-            nowText.Font = new System.Drawing.Font("SimHei", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            nowText.ForeColor = System.Drawing.Color.Green;
-            nowText.Location = new System.Drawing.Point(4, 7);
-            nowText.Margin = new System.Windows.Forms.Padding(2);
-            nowText.Name = "nowText";
-            nowText.ReadOnly = true;
-            nowText.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            nowText.Size = new System.Drawing.Size(85, 23);
-            nowText.TabIndex = 9999;
-            nowText.TabStop = false;
-            nowText.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nowText.BackColor = System.Drawing.SystemColors.Control;
+            this.nowText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nowText.Cursor = System.Windows.Forms.Cursors.No;
+            this.nowText.Font = new System.Drawing.Font("SimHei", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.nowText.ForeColor = System.Drawing.Color.Green;
+            this.nowText.Location = new System.Drawing.Point(4, 7);
+            this.nowText.Margin = new System.Windows.Forms.Padding(2);
+            this.nowText.Name = "nowText";
+            this.nowText.ReadOnly = true;
+            this.nowText.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.nowText.Size = new System.Drawing.Size(85, 23);
+            this.nowText.TabIndex = 9999;
+            this.nowText.TabStop = false;
+            this.nowText.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // instancePanel
             // 
-            instancePanel.AutoSize = true;
-            instancePanel.Dock = System.Windows.Forms.DockStyle.Top;
-            instancePanel.Location = new System.Drawing.Point(3, 198);
-            instancePanel.Margin = new System.Windows.Forms.Padding(2);
-            instancePanel.Name = "instancePanel";
-            instancePanel.Size = new System.Drawing.Size(289, 0);
-            instancePanel.TabIndex = 10;
+            this.instancePanel.AutoSize = true;
+            this.instancePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.instancePanel.Location = new System.Drawing.Point(3, 198);
+            this.instancePanel.Margin = new System.Windows.Forms.Padding(2);
+            this.instancePanel.Name = "instancePanel";
+            this.instancePanel.Size = new System.Drawing.Size(278, 0);
+            this.instancePanel.TabIndex = 10;
             // 
             // bossPanel
             // 
-            bossPanel.AutoSize = true;
-            bossPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            bossPanel.Location = new System.Drawing.Point(3, 198);
-            bossPanel.Margin = new System.Windows.Forms.Padding(2);
-            bossPanel.Name = "bossPanel";
-            bossPanel.Size = new System.Drawing.Size(289, 0);
-            bossPanel.TabIndex = 20;
+            this.bossPanel.AutoSize = true;
+            this.bossPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bossPanel.Location = new System.Drawing.Point(3, 198);
+            this.bossPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.bossPanel.Name = "bossPanel";
+            this.bossPanel.Size = new System.Drawing.Size(278, 0);
+            this.bossPanel.TabIndex = 20;
             // 
             // skillPanel
             // 
-            skillPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            skillPanel.Location = new System.Drawing.Point(3, 35);
-            skillPanel.Margin = new System.Windows.Forms.Padding(2);
-            skillPanel.Name = "skillPanel";
-            skillPanel.MinimumSize = new System.Drawing.Size(289, 163);
-            skillPanel.TabIndex = 0;
-            skillPanel.AutoSize = true;
+            this.skillPanel.AutoSize = true;
+            this.skillPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.skillPanel.Location = new System.Drawing.Point(3, 35);
+            this.skillPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.skillPanel.MinimumSize = new System.Drawing.Size(289, 163);
+            this.skillPanel.Name = "skillPanel";
+            this.skillPanel.Size = new System.Drawing.Size(289, 163);
+            this.skillPanel.TabIndex = 0;
             // 
             // WanmeiTimer
             // 
-            AutoSize = true;
-            AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            Controls.Add(bossPanel);
-            Controls.Add(instancePanel);
-            Controls.Add(skillPanel);
-            Controls.Add(titlePanel);
-            Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            Margin = new System.Windows.Forms.Padding(2);
-            MaximizeBox = false;
-            Name = "WanmeiTimer";
-            Padding = new System.Windows.Forms.Padding(3);
-            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            Text = "计时器";
-            TopMost = true;
-            Load += new System.EventHandler(Form_Load);
-            titlePanel.ResumeLayout(false);
-            titlePanel.PerformLayout();
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.bossPanel);
+            this.Controls.Add(this.instancePanel);
+            this.Controls.Add(this.skillPanel);
+            this.Controls.Add(this.titlePanel);
+            this.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "WanmeiTimer";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "计时器";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.titlePanel.ResumeLayout(false);
+            this.titlePanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
