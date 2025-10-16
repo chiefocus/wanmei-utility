5d9bf5e020313f2ee685c44edbced1ad0dfcaaf9
Feng Chen <chenfeng1104@gmail.com>
2025-10-15 22:19:34 -0400

    improvement

--- Timer/Timer.Designer.cs
+++ Timer/Timer.Designer.cs
@@ -122,7 +122,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 305);
             this.Controls.Add(this.bossPanel);
             this.Controls.Add(this.instancePanel);
             this.Controls.Add(this.skillPanel);
